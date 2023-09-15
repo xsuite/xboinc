675f5b5ae02371c5d05f233c2ff5dd35e54ad415
Frederik Van der Veken <frederik@cern.ch>
2023-09-15 19:53:09 +0200

    Cleaning simulation IO.

--- xboinc/simulation_io/__init__.py
+++ xboinc/simulation_io/__init__.py
@@ -3,5 +3,5 @@
 # Copyright (c) CERN, 2023.                 #
 # ######################################### #
 
-from .simulation_state import SimState
-from .simulation_data import build_input_file, SimConfig
+from .output import SimState
+from .input import SimConfig
