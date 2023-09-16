241461d94a315979263840d78b7c97fc45cf3e1c
Frederik Van der Veken <frederik@cern.ch>
2023-09-16 03:02:41 +0200

    Updated version number to v0.1.0.

--- xboinc/general.py
+++ xboinc/general.py
@@ -12,7 +12,7 @@ _pkg_root = Path(__file__).parent.absolute()
 # Do not change
 # ==========================================================================================
 
-__version__ = '0.0.3'
+__version__ = '0.1.0'
 
 # These are the xsuite modules that are used by boinc and the versions they are tied to.
 # This will be automatically updated from the active environment when making a minor release.
