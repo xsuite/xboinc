bb0ef5da8d53e2c0fadb427d3884961865fb483e
Frederik Van der Veken <frederik@cern.ch>
2023-09-17 17:26:31 +0200

    Updated versioning logic; no need to store patch in SimVersion.

--- xboinc/simulation_io/version.py
+++ xboinc/simulation_io/version.py
@@ -9,28 +9,27 @@ import xpart as xp
 from ..general import __version__
 
 
-# XXX.YYY.ZZZ to XXXYYYZZZ
+# XXX.YYY.ZZZ to XXXYYY  (ignore patch)
 def _version_to_int(version):
     vers = version.split('.')
-    return int(vers[0])*1000000 + int(vers[1])*1000 + int(vers[2])
+    return int(vers[0])*1000 + int(vers[1])
 
-# XXXYYYZZZ to XXX.YYY.ZZZ
+# XXXYYY to XXX.YYY
 def _int_to_version(verint):
-    XXX = int(verint / 1000000)
-    YYY = int((verint - XXX*1000000)/1000)
-    ZZZ = int(verint - XXX*1000000 - YYY*1000)
-    return f"{XXX}.{YYY}.{ZZZ}"
+    XXX = int(verint / 1000)
+    YYY = int(verint - XXX*1000)
+    return f"{XXX}.{YYY}"
 
 
 app_version = '.'.join(__version__.split('.')[:2])
-app_version_int = int(_version_to_int(__version__)/1000)
+app_version_int = _version_to_int(__version__)
 
 
 # This class overloads the first field from the SimState,
 # in order to read the correct version from the binary
 class SimVersion(xo.HybridClass):
     _xofields = {
-        'xboinc_version':   xo.Int64,    # version XXX.YYY.ZZZ as int
+        'xboinc_version': xo.Int64,    # version XXX.YYY as int
     }
 
     def __init__(self, **kwargs):
@@ -39,7 +38,7 @@ class SimVersion(xo.HybridClass):
         super().__init__(**kwargs)
 
     def assert_version(self):
-        if app_version_int != int(self.xboinc_version/1000):
+        if app_version_int != self.xboinc_version:
             error += f"Incompatible xboinc version! Output file needs "
             error +  f"{_int_to_version(self.xboinc_version)}, "
             error +  f"but current version is {__version__}.\n"
