82e8e4b96962be912c83015d5109c18114842ec1
Frederik Van der Veken <frederik@cern.ch>
2024-01-26 02:39:21 +0100

    Added xfields elements to default tracker; this is now possible as compilation of regular Xsuite code is done in C instead of C++. Compilation is slower, though no impact on execution time. Only missing elements are LimitPolygon (PR in Xtrack), ElectronCloud (fieldmap in different buffer), Fringe and Wedge (not implemented), and FlukaCollimator and Geant4Collimator (won't run on BOINC). Will update to new Xsuite versions soon.

--- xboinc/simulation_io/default_tracker.py
+++ xboinc/simulation_io/default_tracker.py
@@ -55,51 +55,47 @@ def get_default_tracker():
         xt.YRotation(),
         xt.ZetaShift(),
         xt.RFMultipole(knl=[0], pn=[0]),
-#        xt.Fringe(),
-#        xt.Wedge(),
+#         xt.Fringe(),
+#         xt.Wedge(),
         xt.DipoleEdge(),
         xt.Exciter(nsamples=1),
         xt.LineSegmentMap(),
         xt.FirstOrderTaylorMap(),
-        # keyword complex (in faddeeva_mit) does not work in C++
-#         xf.BeamBeamBiGaussian2D(
-#             other_beam_Sigma_11=1.,
-#             other_beam_Sigma_33=1.,
-#             other_beam_num_particles=0.,
-#             other_beam_q0=1.,
-#             other_beam_beta0=1.,
-#         ),
-#         # Doesn't work because issue with definition of atomicAdd when generating executable
-#         xf.BeamBeamBiGaussian3D(
-#             slices_other_beam_zeta_center=[0],
-#             slices_other_beam_num_particles=[0],
-#             phi=0.,
-#             alpha=0,
-#             other_beam_q0=1.,
-#             slices_other_beam_Sigma_11=[1],
-#             slices_other_beam_Sigma_12=[0],
-#             slices_other_beam_Sigma_22=[0],
-#             slices_other_beam_Sigma_33=[1],
-#             slices_other_beam_Sigma_34=[0],
-#             slices_other_beam_Sigma_44=[0],
-#         ),
+        xf.BeamBeamBiGaussian2D(
+            other_beam_Sigma_11=1.,
+            other_beam_Sigma_33=1.,
+            other_beam_num_particles=0.,
+            other_beam_q0=1.,
+            other_beam_beta0=1.,
+        ),
+        xf.BeamBeamBiGaussian3D(
+            slices_other_beam_zeta_center=[0],
+            slices_other_beam_num_particles=[0],
+            phi=0.,
+            alpha=0,
+            other_beam_q0=1.,
+            slices_other_beam_Sigma_11=[1],
+            slices_other_beam_Sigma_12=[0],
+            slices_other_beam_Sigma_22=[0],
+            slices_other_beam_Sigma_33=[1],
+            slices_other_beam_Sigma_34=[0],
+            slices_other_beam_Sigma_44=[0],
+        ),
 #         # Doesn't work because fieldmap in different buffer
 #         xf.ElectronCloud(fieldmap=xf.TriCubicInterpolatedFieldMap(
 #             x_range=(0.,1.), nx=10,
 #             y_range=(0.,1.), ny=10,
 #             z_range=(0.,1.), nz=10
 #         )),
-#         # Doesn't work because issue with definition of atomicAdd when generating executable
-#         xf.ElectronLensInterpolated(
-#             x_range=(0.,1.), nx=10,
-#             y_range=(0.,1.), ny=10
-#         ),
-#         # Doesn't work because issue with definition of atomicAdd when generating executable
-#         xf.SpaceCharge3D(
-#             x_range=(0.,1.), nx=10,
-#             y_range=(0.,1.), ny=10,
-#             z_range=(0.,1.), nz=10
-#         ),
+        xf.ElectronLensInterpolated(
+            x_range=(0.,1.), nx=10,
+            y_range=(0.,1.), ny=10
+        ),
+        xf.SpaceCharge3D(
+            x_range=(0.,1.), nx=10,
+            y_range=(0.,1.), ny=10,
+            z_range=(0.,1.), nz=10
+        ),
         xc.BlackAbsorber(length=1),
         xc.EverestCollimator(length=1, material=xc.materials.Silicon),
         xc.EverestCrystal(length=1, material=xc.materials.SiliconCrystal),
