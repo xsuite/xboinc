86a222b491f412aa3c432e347c481134b180c4c0
Frederik F. Van der Veken <frederik@cern.ch>
2024-08-07 23:01:06 +0200

    Compatibility for macOS in version script

--- version.sh
+++ version.sh
@@ -120,9 +120,16 @@ then
     exit 1
 fi
 echo "Adapting version files..."
-sed -i "s/\(int64_t xboinc_exec_version =\).*/\1 "${exec_ver}";/" xboinc/executable/main.c
-sed -i "s/\(__version__ =\).*/\1 '"${new_ver}"'/"                 xboinc/general.py
-sed -i "s/\(assert __version__ ==\).*/\1 '"${new_ver}"'/"         tests/test_00_version.py
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+    sed -i '' "s/\(int64_t xboinc_exec_version =\).*/\1 "${exec_ver}";/" xboinc/executable/main.c
+    sed -i '' "s/\(__version__ =\).*/\1 '"${new_ver}"'/"                 xboinc/general.py
+    sed -i '' "s/\(assert __version__ ==\).*/\1 '"${new_ver}"'/"         tests/test_00_version.py
+else
+    sed -i'' "s/\(int64_t xboinc_exec_version =\).*/\1 "${exec_ver}";/" xboinc/executable/main.c
+    sed -i'' "s/\(__version__ =\).*/\1 '"${new_ver}"'/"                 xboinc/general.py
+    sed -i'' "s/\(assert __version__ ==\).*/\1 '"${new_ver}"'/"         tests/test_00_version.py
+fi
 echo "Committing version change..."
 git reset
 git add pyproject.toml xboinc/general.py tests/test_00_version.py xboinc/executable/main.c
