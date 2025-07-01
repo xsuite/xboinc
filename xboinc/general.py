7e5ab705ba16a3a640bd24ada7c539c80daac635
Carlo Emilio MONTANARI <carlo.emilio.montanari@cern.ch>
2025-07-01 11:11:21 +0200

    Bump version to 0.1.8 and update xsuite dependencies in pyproject.toml; enhance Makefile for xtrack integration and clean up default_tracker.py

--- xboinc/general.py
+++ xboinc/general.py
@@ -13,20 +13,19 @@ _pkg_root = Path(__file__).parent.absolute()
 # Do not change
 # ==========================================================================================
 
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 # These are the xsuite modules that are used by boinc and the versions they are tied to.
 # This will be automatically updated from the active environment when making a minor release.
 # If a new package needs to be pinned, add it here with a random version number,
 # and similarily in the pyproject.toml
 __xsuite__versions__ = {
-    'xobjects' : '0.2.10',
-    'xdeps'    : '0.5.2',
-    'xpart'    : '0.16.3',
-    'xtrack'   : '0.48.1',
-    'xfields'  : '0.15.0',
-    'xcoll'    : '0.2.7',
-    'xaux'     : '0.1.2',
+    'xobjects' : '0.5.0',
+    'xdeps'    : '0.10.5',
+    'xpart'    : '0.23.0',
+    'xtrack'   : '0.84.7',
+    'xfields'  : '0.24.0',
+    'xcoll'    : '0.6.1',
+    'xaux'     : '0.3.5',
 }
-
 # ==========================================================================================
