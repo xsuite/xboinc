6f0636fb1001eed96cc9dd803c4d39df4573e90e
Frederik Van der Veken <frederik@cern.ch>
2025-08-29 11:44:32 +0200

    Created release branch release/v0.4.3rc0.

--- tests/test_version.py
+++ tests/test_version.py
@@ -12,7 +12,7 @@ from xboinc.simulation_io.version import _version_to_int, _int_to_version, asser
 
 
 def test_version():
-    assert __version__ == '0.4.2'
+    assert __version__ == '0.4.3rc0'
 
 
 def test_xb_ver():
