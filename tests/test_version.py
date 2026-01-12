db418bdbe5f0c118db0fe2e7913b20b9f58240b5
Frederik F. Van der Veken <frederik@cern.ch>
2026-01-12 14:59:15 +0100

    Small typo

--- tests/test_version.py
+++ tests/test_version.py
@@ -25,13 +25,13 @@ def test_xb_ver():
 
 def test_xsuite_versions():
     expected_version = {
-        'xobjects' : '0.5.12'
-        'xdeps'    : '0.10.10'
-        'xpart'    : '0.23.7'
-        'xtrack'   : '0.98.12'
-        'xfields'  : '0.25.8'
-        'xcoll'    : '0.9.1'
-        'xaux'     : '0.3.10'
+        'xobjects' : '0.5.12',
+        'xdeps'    : '0.10.10',
+        'xpart'    : '0.23.7',
+        'xtrack'   : '0.98.12',
+        'xfields'  : '0.25.8',
+        'xcoll'    : '0.9.1',
+        'xaux'     : '0.3.10',
     }
     current_version = {}
 
